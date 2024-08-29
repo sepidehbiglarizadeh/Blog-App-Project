000ae84b69416a6463c10de37e073c3932a1afd1
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2024-08-29 13:27:17 +0330

    Adding Fonts to Next

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 function withOpacity(variableName) {
   return ({ opacityValue }) => {
@@ -41,6 +42,9 @@ module.exports = {
         success: withOpacity("--color-success"),
         warning: withOpacity("--color-warning"),
         error: withOpacity("--color-error"),
+      },
+      fontFamily: {
+        sans: ["var(--font-vazir)", ...fontFamily.sans],
       },
     },
   },
