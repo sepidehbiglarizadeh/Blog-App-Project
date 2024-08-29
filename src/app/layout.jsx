c51960b5e24bfc077d78a2570703a62331d8dcd1
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2024-08-29 13:46:39 +0330

    Home Page

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,4 @@
+import Header from "@/components/Header";
 import vazirFont from "@/constants/localFont";
 import "@/styles/globals.css";
 
@@ -9,7 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
-      <body className={`${vazirFont.variable} font-sans`}>{children}</body>
+      <body className={`${vazirFont.variable} font-sans min-h-screen`}>
+        <Header />
+        <div className="container xl:max-w-screen-xl">{children}</div>
+      </body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -46,6 +46,10 @@ module.exports = {
       fontFamily: {
         sans: ["var(--font-vazir)", ...fontFamily.sans],
       },
+      container: {
+        center: true,
+        padding: "1rem",
+      },
     },
   },
   plugins: [],
