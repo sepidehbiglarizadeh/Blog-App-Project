2dc61b567b23dd466891b73ce7983e958de58570
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2024-08-31 14:56:27 +0330

    post content

--- src/app/blogs/_components/PostList.jsx
+++ src/app/blogs/_components/PostList.jsx
@@ -1,4 +1,9 @@
 import Image from "next/image";
+import CoverImage from "./CoverImage";
+import Link from "next/link";
+import { ClockIcon } from "@heroicons/react/24/outline";
+import Avatar from "@/ui/Avatar";
+import Author from "./Author";
 
 async function PostList() {
   await new Promise((resolve) => setTimeout(() => resolve(), 3000));
@@ -15,14 +20,25 @@ async function PostList() {
           className="col-span-12 sm:col-span-6 lg:col-span-4 border border-secondary-300 p-2 rounded-lg"
           key={post._id}
         >
-          <div className="relative aspect-video overflow-hidden rounded-md mb-6">
-            <Image
-              src={post.coverImageUrl}
-              alt={post.title}
-              fill
-              className="object-cover object-center hover:scale-110 transition-all duration-300 ease-out"
-              quality={80}
-            />
+          <CoverImage {...post} />
+          {/* post content */}
+          <div>
+            <Link href={`/blogs/${post.slug}`}>
+              <h2 className="mb-4 font-bold text-secondary-700">
+                {post.title}
+              </h2>
+            </Link>
+
+            {/* post author - reading time */}
+            <div className="flex items-center justify-between">
+              <Author {...post.author} />
+              <div className="flex items-center text-[10px] text-secondary-500">
+                <ClockIcon className="w-4 h-4 stroke-secondary-500 ml-1" />
+                <span className="ml-1">خواندن : </span>
+                <span className="ml-1 leading-3">{post.readingTime}</span>
+                <span>دقیقه</span>
+              </div>
+            </div>
           </div>
         </div>
       ))}
