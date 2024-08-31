828413924858577e57855297ecd9e23bd1c23dc2
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2024-08-31 13:46:00 +0330

    intro to Next.js Image component

--- src/app/blogs/_components/PostList.jsx
+++ src/app/blogs/_components/PostList.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 async function PostList() {
   await new Promise((resolve) => setTimeout(() => resolve(), 3000));
 
@@ -6,9 +8,18 @@ async function PostList() {
     data: { posts },
   } = await res.json();
 
-  return posts.length > 0
-    ? posts.map((post) => <div key={post._id}>{post.title}</div>)
-    : null;
+  return posts.length > 0 ? (
+    <div className="grid grid-cols-12 gap-8">
+      {posts.map((post) => (
+        <div
+          className="col-span-12 sm:col-span-6 lg:col-span-4 border border-secondary-100 p-2 rounded-lg"
+          key={post._id}
+        >
+          <Image src={post.coverImageUrl} alt="" width={400} height={500} />
+        </div>
+      ))}
+    </div>
+  ) : null;
 }
 
 export default PostList;
