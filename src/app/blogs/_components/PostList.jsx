efb9a58b0248cd45e374d0c5c7587300f0b17946
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2024-09-04 13:33:40 +0330

    Post interaction

--- src/app/blogs/_components/PostList.jsx
+++ src/app/blogs/_components/PostList.jsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { ClockIcon } from "@heroicons/react/24/outline";
 import Avatar from "@/ui/Avatar";
 import Author from "./Author";
+import PostInteraction from "./PostInteraction";
 
 async function PostList() {
   await new Promise((resolve) => setTimeout(() => resolve(), 3000));
@@ -30,7 +31,7 @@ async function PostList() {
             </Link>
 
             {/* post author - reading time */}
-            <div className="flex items-center justify-between">
+            <div className="flex items-center justify-between mb-4">
               <Author {...post.author} />
               <div className="flex items-center text-[10px] text-secondary-500">
                 <ClockIcon className="w-4 h-4 stroke-secondary-500 ml-1" />
@@ -39,6 +40,7 @@ async function PostList() {
                 <span>دقیقه</span>
               </div>
             </div>
+            <PostInteraction post={post} />
           </div>
         </div>
       ))}
