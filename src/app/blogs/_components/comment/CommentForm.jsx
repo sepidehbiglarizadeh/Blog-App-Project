c4c43cf501bf1f9dd16c7c490f03e8cf3a652ed9
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2024-09-17 11:19:17 +0330

    Server Actions in Next.js

--- src/app/blogs/_components/comment/CommentForm.jsx
+++ src/app/blogs/_components/comment/CommentForm.jsx
@@ -1,5 +1,6 @@
-'use client'
+"use client";
 
+import { createComment } from "@/lib/actions";
 import Button from "@/ui/Button";
 import TextArea from "@/ui/TextArea";
 import { useState } from "react";
@@ -7,11 +8,13 @@ import { useState } from "react";
 function CommentForm() {
   const [text, setText] = useState("");
 
+ 
+
   return (
     <div>
       <div className="flex justify-center mt-4">
         <div className="max-w-md  w-full">
-          <form className="space-y-7">
+          <form className="space-y-7" action={createComment}>
             <TextArea
               name="text"
               label="متن نظر"
