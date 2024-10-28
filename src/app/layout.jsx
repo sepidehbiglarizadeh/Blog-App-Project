924fcb8cef50632f1243ebdf4224756b13ea69c1
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2024-10-28 18:13:14 +0330

    create nasted layout for dashboard

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,3 @@
-import Header from "@/components/Header";
 import vazirFont from "@/constants/localFont";
 import AuthProvider from "@/context/AuthContext";
 import "@/styles/globals.css";
@@ -17,11 +16,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body className={`${vazirFont.variable} font-sans min-h-screen`}>
-        <AuthProvider>
-          <Toaster />
-          <Header />
-          <div className="container xl:max-w-screen-xl">{children}</div>
-        </AuthProvider>
+        <Toaster />
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
