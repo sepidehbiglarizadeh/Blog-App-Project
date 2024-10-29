2eb1e0f37b1fe65ae3cb3ef3eb5d199872f12278
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2024-10-29 11:11:26 +0330

    Create Drawer

--- src/app/(dashboard)/profile/_components/Header.jsx
+++ src/app/(dashboard)/profile/_components/Header.jsx
@@ -3,32 +3,39 @@ import Link from "next/link";
 import { useAuth } from "@/context/AuthContext";
 import ButtonIcon from "@/ui/ButtonIcon";
 import Avatar from "@/ui/Avatar";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import SideBar from "./SideBar";
+import { useState } from "react";
+import Drawer from "@/ui/Drawer";
 
 function Header({}) {
-  // const [isOpenDrawer, setIsOpenDrawer] = useState(false);
+  const [isOpenDrawer, setIsOpenDrawer] = useState(false);
   const { user, isLoading } = useAuth();
+
   return (
     <header
       className={`bg-secondary-0 ${isLoading ? "bg-opacity-30 blur-md" : ""}`}
     >
       <div className="flex items-center justify-between py-5 px-4 lg:px-8">
-        <div className="flex items-center gap-x-3">
-          <div className="flex flex-col lg:flex-row justify-start lg:items-center gap-x-2">
-            <span className="text-sm lg:text-lg font-bold text-secondary-700">
-              سلام؛ {user?.name}
-            </span>
-          </div>
-        </div>
-        <div className="flex items-center gap-x-3">
-          <Link href="/profile">
-            <ButtonIcon
-              color="outline"
-              className={`border-secondaray-200 rounded-2xl flex cursor-pointer items-center`}
-            >
-              <Avatar src={user?.avatarUrl} />
-            </ButtonIcon>
-          </Link>
-        </div>
+        <ButtonIcon
+          className="block lg:hidden border-none"
+          variant="outline"
+          onClick={() => setIsOpenDrawer(!isOpenDrawer)}
+        >
+          {isOpenDrawer ? <XMarkIcon /> : <Bars3Icon />}
+        </ButtonIcon>
+
+        <span className="text-sm lg:text-lg font-bold text-secondary-700">
+          سلام؛ {user?.name}
+        </span>
+
+        <Link href="/profile">
+          <Avatar src={user?.avatarUrl} />
+        </Link>
+
+        <Drawer open={isOpenDrawer} onClose={() => setIsOpenDrawer(false)}>
+          <SideBar onClose={() => setIsOpenDrawer(false)} />
+        </Drawer>
       </div>
     </header>
   );
--- src/app/(dashboard)/profile/_components/SideBar.jsx
+++ src/app/(dashboard)/profile/_components/SideBar.jsx
@@ -4,25 +4,35 @@ import { useAuth } from "@/context/AuthContext";
 import {
   ArrowLeftStartOnRectangleIcon,
   HomeIcon,
+  XMarkIcon,
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import SideBarNavs from "./SideBarNavs";
+import ButtonIcon from "@/ui/ButtonIcon";
 
-function SideBar() {
+function SideBar({ onClose }) {
   const { logout } = useAuth();
-
 
   return (
     <div className="overflow-y-auto flex flex-col p-5 h-screen pt-10 lg:pt-8">
       {/* Drawer header */}
-      <Link
-        href="/"
-        className="flex items-center gap-x-4 justify-center text-secondary-700 border-b  border-b-secondary-200 
+      <div className="flex items-center justify-between w-full mb-5 pb-2 border-b border-b-secondary-200">
+        <Link
+          href="/"
+          className="flex items-center gap-x-4 justify-center text-secondary-700 border-b  border-b-secondary-200 
         pb-2 mb-6"
-      >
-        <HomeIcon className="w-6 h-6" />
-        <span> نکست بلاگ</span>
-      </Link>
+        >
+          <HomeIcon className="w-6 h-6" />
+          <span> نکست بلاگ</span>
+        </Link>
+        <ButtonIcon
+          onClick={onClose}
+          className="block lg:hidden border-none"
+          variant="outline"
+        >
+          <XMarkIcon />
+        </ButtonIcon>
+      </div>
       {/* Drawer content */}
       <div className="overflow-y-auto flex-auto">
         <SideBarNavs />
