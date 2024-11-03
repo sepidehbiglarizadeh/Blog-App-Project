efaaee9c401f47218ad0a33b7192e40483236ebd
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2024-11-03 20:15:06 +0330

    Bread Crumbs

---
+++ src/app/(dashboard)/profile/posts/create/page.jsx
@@ -0,0 +1,21 @@
+import Breadcrumbs from "@/ui/Breadcrumbs";
+
+export default function page() {
+  return (
+    <div>
+      <Breadcrumbs
+        breadcrumbs={[
+          {
+            label: "پست ها",
+            href: "/profile/posts",
+          },
+          {
+            label: "ایجاد پست",
+            href: "/profile/posts/create",
+            active: true,
+          },
+        ]}
+      />
+    </div>
+  );
+}
