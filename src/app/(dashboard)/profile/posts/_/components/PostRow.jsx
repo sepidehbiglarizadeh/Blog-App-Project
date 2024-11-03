ec464da9abb9aa5509ef0bcfcab43ba275292f38
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2024-11-03 13:42:19 +0330

    Post row

---
+++ src/app/(dashboard)/profile/posts/_/components/PostRow.jsx
@@ -0,0 +1,38 @@
+import Table from "@/ui/Table";
+import { toLocalDateShort } from "@/utils/dateFormatter";
+import { toPersianDigits } from "@/utils/numberFormatter";
+import truncateText from "@/utils/trancateText";
+import classNames from "classnames";
+
+const typeStyle = {
+  free: {
+    label: "رایگان",
+    className: "badge--success",
+  },
+  premium: {
+    label: "پولی",
+    className: "badge--secondary",
+  },
+};
+
+function PostRow({ index, post }) {
+  const { title, category, author, createdAt, type } = post;
+
+  return (
+    <Table.Row>
+      <td>{toPersianDigits(index + 1)}</td>
+      <td>{truncateText(title, 30)}</td>
+      <td>{category.title}</td>
+      <td>{author.name}</td>
+      <td>{toLocalDateShort(createdAt)}</td>
+      <td>
+        <span className={`badge ${typeStyle[type].className}`}>
+          {typeStyle[type].label}
+        </span>
+      </td>
+      <td>actions...</td>
+    </Table.Row>
+  );
+}
+
+export default PostRow;
